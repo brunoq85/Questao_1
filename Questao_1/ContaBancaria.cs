36f8e6840ba989bafdadab9889427a847d391922
brunoq85 <38147160+brunoq85@users.noreply.github.com>
2024-07-03 16:00:21 -0300

    Adicionando o código na classe Program. Refatorando a classe ContaBancaria

--- Questao_1/ContaBancaria.cs
+++ Questao_1/ContaBancaria.cs
@@ -1,9 +1,27 @@
-﻿namespace Questao_1
+﻿
+namespace Questao_1
 {
     class ContaBancaria
     {
         public int NumeroConta { get; private set; }
         public string NomeTitular { get; set; }
         private double Saldo { get; set; }
+
+        public ContaBancaria(int numeroConta, string nomeTitular, double depositoInicial = 0)
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular;
+            Depositar(depositoInicial);
+        }
+
+        public void Depositar(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Sacar(double quantia)
+        {
+            Saldo -= (quantia + 3.50);
+        }
     }
 }
