d69925007d84518b4992d33c6657202eceb08a7f
brunoq85 <38147160+brunoq85@users.noreply.github.com>
2024-07-03 15:24:17 -0300

    Add project files.

---
+++ Questao_1/ContaBancaria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao_1
+{
+    class ContaBancaria
+    {
+        public int NumeroConta { get; private set; }
+        public string NomeTitular { get; set; }
+        private double Saldo { get; set; }
+    }
+}
