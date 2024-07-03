452ad2af2183a13a0fe0334e814b15532778f8f1
brunoq85 <38147160+brunoq85@users.noreply.github.com>
2024-07-03 15:26:07 -0300

    Criando a classe conta bancária

--- Questao_1/ContaBancaria.cs
+++ Questao_1/ContaBancaria.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Questao_1
+﻿namespace Questao_1
 {
     class ContaBancaria
     {
